aa89d1fdc1522b0180522e662c7e3844dd22a76c
JanaathV <janaath@live.com>
2021-07-14 22:34:54 -0400

    first commit

---
+++ Server/Models/match.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema; // alias for the mongoose schema
+
+const MatchItemSchema = new Schema
+({
+    matchNumber: String,
+    competitor1Name: String,
+    competitor2Name: String,
+    winner: String
+},
+{
+    collection: "match"
+});
+
+const Model = mongoose.model("Match", MatchItemSchema);
+export default Model;
